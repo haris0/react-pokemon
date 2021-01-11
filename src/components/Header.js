9d963e5d907a2ec83f0255ab85dd3c8b5d3c4788
LAPTOP-493LRSOQ\haris <hari.surrisyad@gmail.com>
2021-01-11 08:58:12 +0700

    create header card and pokemon list page

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {Image, Flex} from "@chakra-ui/react";
+import Poketop from "../assets/Poketop.png";
+
+
+export default function Header(props){
+  return (
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      overflow="hidden"
+      position="fixed"
+      top="0"
+      width="100%"
+      wrap="wrap"
+      padding="1rem"
+      color="teal"
+      boxShadow="lg"
+      marginBottom="15px"
+      {... props}
+    >
+      <Flex align="center" mr={5}>
+        <Image
+          width="120px"
+          src={Poketop}
+        />
+      </Flex>
+    </Flex>
+  );
+};
---
+++ src/containers/PokemonList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from "../components/Header";
+import { Container } from "@chakra-ui/react";
+
+export default function PokemonList() {
+  return (
+    <>
+      <Header />
+      <Container maxW="960px" marginTop="75px">
+      </Container>
+    </>
+  );
+}
